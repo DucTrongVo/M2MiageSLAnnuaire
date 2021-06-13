8f64c5fa6ee40dc88343833d17f420c46d1994d6
DucTrongVo <duc-trong.vo@toulouse.miage.fr>
2021-06-13 23:20:02 +0200

    annuaire

---
+++ src/test/java/m2/miage/msl/annuaire/AnnuaireApplicationTests.java
@@ -0,0 +1,13 @@
+package m2.miage.msl.annuaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
